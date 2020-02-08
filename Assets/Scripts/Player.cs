9fe7da57010cf7bc3ea6d5d75beb444692d56976
Evan Hong <evangelurtz@gmail.com>
2020-02-08 15:23:18 +0900

    Add character controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController2D controller;
+    private float horizontalMove = 0f;
+    [SerializeField]
+    private float runSpeed = 40f;
+    private bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        if (jump)
+        {
+            jump = false;
+        }
+    }
+}
