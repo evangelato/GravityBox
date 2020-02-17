9af6445393aa41b86e3086d7ab88f5b85feeb4a7
Evan Hong <evangelurtz@gmail.com>
2020-02-17 23:10:29 +0900

    Add jump and fall animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,13 +9,17 @@ public class Player : MonoBehaviour
     [SerializeField] private float runSpeed = 40f;
     private bool jump = false;
     [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody2D ridigBody2D;
     private bool isGravityFlipped = false;
+    private bool isFalling = false;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        animator.SetBool("IsFalling", isFalling);
+        animator.SetBool("IsJumping", jump);
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
@@ -32,6 +36,8 @@ public class Player : MonoBehaviour
             transform.Rotate(180f, 0f, 0f, Space.Self);
             isGravityFlipped = false;
         }
+
+        CheckFallCondition();
     }
 
     void FixedUpdate()
@@ -42,7 +48,35 @@ public class Player : MonoBehaviour
             jump = false;
         }
     }
-    
+
+    void CheckFallCondition()
+    {
+        if (!isGravityFlipped)
+        {
+            if (ridigBody2D.velocity.y < -0.1)
+            {
+                isFalling = true;
+            }
+            else
+            {
+                isFalling = false;
+            }
+        } 
+        else
+        {
+            if (ridigBody2D.velocity.y > 0.1)
+            {
+                isFalling = true;
+            }
+            else
+            {
+                isFalling = false;
+            }
+        } 
+
+
+    }
+
     public bool IsGravityFlipped()
     {
         return isGravityFlipped;
